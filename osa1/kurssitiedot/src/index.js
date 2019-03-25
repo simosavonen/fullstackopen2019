76d0894d16df93b67b39905a79d8b96f68943c87
Simo Savonen <sipesa@utu.fi>
2019-03-25 21:38:51 +0200

    kurssitiedot, step 5

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -36,27 +36,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-    const parts = [
-        {
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+            {
             name: 'Reactin perusteet',
             exercises: 10
-        },
-        {
+            },
+            {
             name: 'Tiedonvälitys propseilla',
             exercises: 7
-        },
-        {
+            },
+            {
             name: 'Komponenttien tila',
             exercises: 14
-        }
-    ]
+            }
+        ]
+    }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
