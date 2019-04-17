2d009e26b6deba7442862ca2fc3d302b906e8048
Simo Savonen <sipesa@utu.fi>
2019-04-17 18:53:36 +0300

    6.3 6.4 anekdootit step1 ja 2

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
+
+const App = (props) => {
+  const store = props.store
+  const anecdotes = store.getState()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    store.dispatch(voteAnecdote(id))
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    store.dispatch(createAnecdote(event.target.anecdote.value))
+    event.target.anecdote.value = ''
+  }
+
+
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
