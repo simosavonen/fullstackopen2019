001fdde3747572a05a1242cc3c15eb471117b5f0
Simo Savonen <sipesa@utu.fi>
2019-04-25 19:06:25 +0300

    8.3 kaikki kirjailijat

--- osa8/index.js
+++ osa8/index.js
@@ -88,6 +88,7 @@ const typeDefs = gql`
     name: String!
     id: ID!
     born: Int
+    bookCount: Int
   }
 
   type Book {
@@ -102,6 +103,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors:[Author!]!
   }
 `
 
@@ -109,9 +111,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
+
 
 const server = new ApolloServer({
   typeDefs,
