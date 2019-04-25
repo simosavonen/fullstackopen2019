a670bfe47e0b95f8cfd5845dc42fcbd42cc0154a
Simo Savonen <sipesa@utu.fi>
2019-04-25 18:04:45 +0300

    8.2 kaikki kirjat ja kirjailijat

--- osa8/index.js
+++ osa8/index.js
@@ -93,7 +93,7 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: Author!
+    author: String!
     id: ID!
     genres: [String]!
   }
@@ -101,13 +101,15 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
